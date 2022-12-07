35f9d38f5c3ec4a7f4bc1a38276411b53d112089
probir ghosh <probirghosh@outlook.com>
2022-12-07 11:48:21 +0600

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
-function App() {
+
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* props -> variant ="text/undefined/contained/outlined" */}
+      <Button variant="outlined">Hello Material UI</Button>
+
     </div>
   );
 }
-
-export default App;
